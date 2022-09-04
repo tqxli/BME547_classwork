a53da731f1fa7d6631b4881342526c6e6e9b7104
tqxli <tianqing.li@duke.edu>
2022-09-04 13:31:29 -0400

    add a function that checks the HDL result

--- interface.py
+++ interface.py
@@ -2,6 +2,14 @@ def get_user_input():
     level = input("Enter: ")
     return level
 
+def check_HDL(hdl):
+    if hdl >= 60:
+        return "Normal"
+    elif hdl >= 40:
+        return "Borderline Low"
+    else:
+        return "Low"
+
 def interface():
     print("My Program")
     print("Options:")
