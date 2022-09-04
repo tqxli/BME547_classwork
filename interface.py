8903d0b08f0448eda4984eff910e131f4c88018d
tqxli <tianqing.li@duke.edu>
2022-09-04 13:19:55 -0400

    modify interface so that it continues until quit

---
+++ interface.py
@@ -0,0 +1,10 @@
+def interface():
+    print("My Program")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    while choice != '9':
+        choice = input("Enter your choice: ")
+    return
+   
+interface()
