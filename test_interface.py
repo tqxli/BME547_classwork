d6dcb58622b91ffec8c517c2c2cf35c55f4ab8c4
tqxli <tianqing.li@duke.edu>
2022-09-20 11:01:17 -0400

    add unit test for check_ldl

--- test_interface.py
+++ test_interface.py
@@ -9,4 +9,16 @@ def test_check_HDL(level, expected):
     from interface import check_HDL
 
     answer = check_HDL(level)
-    assert answer == expected  
+    assert answer == expected 
+
+@pytest.mark.parametrize("level, expected", [
+    (120, "Normal"),
+    (150, "Borderline High"),
+    (185, "High"),
+    (240, "Very High")
+])
+def test_check_LDL(level, expected):
+    from interface import check_LDL
+
+    answer = check_LDL(level)
+    assert answer == expected   
